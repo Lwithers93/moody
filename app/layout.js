1dd42f446ad1753edecffb0f9b1244e80a45130f
Luke <lwithers93@live.co.uk>
2025-01-10 13:18:02 +0000

    Update favicon

--- app/layout.js
+++ app/layout.js
@@ -37,6 +37,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.png" />
       {/* wrap entire application in Auth Provider  */}
 
       <body
